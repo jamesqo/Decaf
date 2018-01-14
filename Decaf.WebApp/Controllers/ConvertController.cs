75d4fe2a84fbc56362fa945602c86c3657715e22
James Ko <jamesqko@gmail.com>
2018-01-14 17:45:22 -0500

    First attempt at a webapp

---
+++ Decaf.WebApp/Controllers/ConvertController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Decaf.WebApp.Controllers
+{
+    public class ConvertController : Controller
+    {
+        [HttpGet]
+        public string Get(
+            string javaCode)
+        {
+            // TODO: Unqualified name
+            return CoffeeMachine.Decaf.Brew(javaCode);
+        }
+    }
+}
